084f51401a7e94f09fb26bfefb43826d8935f2fd
chintanpatel <chintanpatel9065@gmail.com>
2025-06-18 11:34:20 +0530

    Initialize a Maven-based Spring Boot project for DropdownListApp. Includes configurations, dependencies, wrappers, and sample test.

---
+++ src/main/java/com/example/dropdownlistapp/DropdownListAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dropdownlistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropdownListAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropdownListAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dropdownlistapp/DropdownListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dropdownlistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropdownListAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
